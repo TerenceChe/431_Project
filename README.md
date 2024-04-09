6d52a7389926036b9ab7e4daa34018b5a04f275f
Lily <lilyyanglt@gmail.com>
2024-04-08 21:30:37 -0700

    updated README and also python script to make it easier to generate test graph

--- README.md
+++ README.md
@@ -4,9 +4,38 @@ Parallel all pairs shortest path algorithm
 # Input Graph Data
 * to test the system, you would need to first generate the data you want and the expectation is that the graph is going to be weighted directed/undirected graph. If there's no path from A -> B, then you should represent the weight as `i` which means infinity. 
 
+For simplicity, we've created a python script called random_graphy.py which will allow you to create a graph of any size to test the program with, to generate input graphs, here's the command
+
+```
+python3 input_graph/random_graph.py 100 ./input_graph/random_graph_100.txt
+
+```
+
 ## Running it
 ```
 make all
-./floyd_serial_threaded --inputFile=./input_graph/random_graph.txt --outputFile=./output_graph/random_graph_serial_threaded.txt
-./floyd_distrib --inputFile=./input_graph/random_graph.txt --outputFile=./output_graph/random_graph_distrib_.txt
+
+# running the serial version using random_graph_1000.txt
+./floyd_serial_threaded --mode 0 --inputFile=./input_graph/random_graph.txt --outputFile=./output_graph/random_graph_serial_threaded.txt
+
+# running the parallel version using random_graph_1000.txt with 4 threads
+./floyd_serial_threaded --np 4 --mode 1 --inputFile=./input_graph/random_graph.txt --outputFile=./output_graph/random_graph_serial_threaded.txt
+
+```
+
+In order to run this inside slurm using 4 processes, you would use the following script file
+
+
+```
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=5G
+#SBATCH --partition=slow
+#SBATCH --output=a6_heat_transfer_results.txt
+
+mpirun -n 4 ./floyd_distrib --inputFile=./input_graph/random_graph.txt --outputFile=./output_graph/random_graph_distrib_.txt
+
 ```
--- input_graph/random_graph.py
+++ input_graph/random_graph.py
@@ -1,25 +1,35 @@
 import random
+import sys
 
-# How many nodes.
-N = 1000
-
-# 70% chance of writing i, 30% of writing a valid integer.
-# Returns a random string to write, either 'i' or some number like '123'.
 def random_to_write() -> str:
-    # INT_MAX = 0x7fffffff # NOTE do we want our code to be safe and handle large values?
     number_probability = 0.3
     if random.random() < number_probability:
-        # return str(random.randint(0, INT_MAX - 1)) # Causes negative overflow.
         return str(random.randint(0, 1000))
     else:
         return 'i'
-    
-with open('random_graph.txt', 'w') as f:
-    for row in range(N):
-        for col in range(N):
-            if row == col:
-                f.write('0')
-            else:
-                f.write(random_to_write())
-            f.write(' ')
-        f.write('\n')
+
+def generate_random_graph(N, filename):
+    with open(filename, 'w') as f:
+        for row in range(N):
+            for col in range(N):
+                if row == col:
+                    f.write('0')
+                else:
+                    f.write(random_to_write())
+                f.write(' ')
+            f.write('\n')
+
+## simple command line to generate input files to create matrix of desired size
+# N - is the number of nodes
+# output file name
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py N input_absolute_filepath")
+    else:
+        try:
+            N = int(sys.argv[1])
+            filename = sys.argv[2]
+            generate_random_graph(N, filename)
+            print(f"Random graph with {N} nodes generated successfully in {filename}.")
+        except ValueError:
+            print("Error: N must be an integer.")
