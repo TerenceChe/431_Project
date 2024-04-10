615f994b36a5700c7ddf834a8dfc3506f1b90c7b
bensmus <bensmus@gmail.com>
2024-04-09 22:54:43 -0700

    fixed comments, improved dynamic memory alloc, prints time

--- floyd_distrib.cpp
+++ floyd_distrib.cpp
@@ -1,8 +1,8 @@
 // Process 0 is leader process and reads input file into matrix.
-// Then it sends submatrices (by column spits) to follower processes.
-// Follower processes update distances in submatrices using floyd algorithm.
-// Then follower processes send submatrices back to leader process.
-// The leader process combines the submatrices into a matrix and writes to output file.
+// Then it sends submatrices (by column spits, called `proc_buffer`) to follower processes.
+// Follower processes update distances in `proc_buffer` using floyd algorithm.
+// Then follower processes send `proc_buffer` back to leader process.
+// The leader process combines the `proc_buffer`s into a matrix and writes to output file.
 
 #include <iostream>
 #include <vector>
@@ -12,6 +12,7 @@
 #include "utils.h"
 #include "floyd_distrib.h"
 
+// Return broadcaster process rank for a certain k (it has column k in its proc_buffer).
 int get_broadcaster(std::vector<int> col_bounds, int k) {
     uint i;
     for (i = 1; i < col_bounds.size(); i++) {
@@ -34,20 +35,28 @@ void distrib(std::string input_file_path, std::string output_file_path) {
     int num_verts = 0;
     Graph *graph = NULL;
 
+    timer root_timer;
+    double *proc_times = NULL;
     if (world_rank == 0) {
         graph = new Graph();
         graph->readGraphFromFile(input_file_path);
         num_verts = graph->getNumVerts();
+        proc_times = new double[world_rank];
+        root_timer.start();
     }
     
+    timer proc_timer;
+    proc_timer.start();
+
     MPI_Bcast(&num_verts, 1, MPI_INT, 0, MPI_COMM_WORLD);
     std::vector<int> col_bounds = get_col_bounds(num_verts, world_size);
 
     int proc_start_col = col_bounds[world_rank];
     int proc_end_col = col_bounds[world_rank + 1];
     int proc_width = proc_end_col - proc_start_col;
+    int proc_width_max = num_verts / world_size + 1;
     int proc_buffer_len = proc_width * num_verts;
-    // Goal is to split graph and each proc (including root) has a bunch of data in here.
+    // Goal is to split graph so that each proc (including root) has its data in proc_buffer.
     int *proc_buffer = new int[proc_buffer_len]; 
 
     if (world_rank == 0) {
@@ -61,13 +70,14 @@ void distrib(std::string input_file_path, std::string output_file_path) {
             }
         }
 
-        // Send some of the graph to all of the other processes:
+        // Send graph segments to all of the other processes:
+        int *other_buffer = new int[proc_width_max * num_verts];
         for (int other_proc = 1; other_proc < world_size; other_proc++) {
             int other_start_col = col_bounds[other_proc];
             int other_end_col = col_bounds[other_proc + 1];
             int other_buffer_len = (other_end_col - other_start_col) * num_verts;
 
-            int *other_buffer = new int[other_buffer_len]; // Submatrix to send to recv_proc.
+            
             int other_buffer_i = 0;
             for (int i = 0; i < num_verts; i++) {
                 for (int j = other_start_col; j < other_end_col; j++) {
@@ -77,8 +87,8 @@ void distrib(std::string input_file_path, std::string output_file_path) {
                 }
             }
             MPI_Send(other_buffer, other_buffer_len, MPI_INT, other_proc, 0, MPI_COMM_WORLD);
-            delete[] other_buffer;
         }
+        delete[] other_buffer;
     } else {
         // Receive graph section from leader process.
         MPI_Recv(proc_buffer, proc_buffer_len, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -92,9 +102,6 @@ void distrib(std::string input_file_path, std::string output_file_path) {
             // Fill kth column from proc_buffer:
             for (int i = 0; i < num_verts; i++) {
                 kth_column[i] = proc_buffer[i * proc_width + (k - proc_start_col)];
-                // int d = proc_buffer[i * proc_width + (k - proc_start_col)];
-                // kth_column[i] = d;
-                // printf("proc %d puts %d into %d column\n", world_rank, d, k);
             }
         }
         // After bcast, all procs will have kth_column with same data.
@@ -111,25 +118,9 @@ void distrib(std::string input_file_path, std::string output_file_path) {
                 int old_dist = proc_buffer[i * proc_width + j];
                 if (new_dist < old_dist) {
                     proc_buffer[i * proc_width + j] = new_dist;
-                    // printf("i=%d, j=%d, k=%d, distto=%d, distfrom=%d\n", i, j + proc_start_col, k, dist_to_k, dist_from_k);
-                    // fflush(stdout);
                 }
             }
         }
-        // MPI_Barrier(MPI_COMM_WORLD);
-        // std::printf("k = %d\n", k);
-        // std::printf("proc_buffer\n");
-        // for (int i = 0; i < num_verts; i++) {
-        //     for (int j = 0; j < proc_width; j++) {
-        //         std::printf("%d ", proc_buffer[i * proc_width + j]);
-        //     }
-        //     std::printf("\n");
-        // }
-        // std::printf("kth column\n");
-        // for (int i = 0; i < num_verts; i++) {
-        //     std::printf("%d ", kth_column[i]);
-        // }
-        // std::printf("\n---\n");
         MPI_Barrier(MPI_COMM_WORLD);
     }
     delete[] kth_column;
@@ -137,10 +128,10 @@ void distrib(std::string input_file_path, std::string output_file_path) {
 
     std::vector<std::vector<int>> combined;
     if (world_rank != 0) {
-        // Done Floyd, send proc_buffer back to root.
+        // Send proc_buffer back to root.
         MPI_Send(proc_buffer, proc_buffer_len, MPI_INT, 0, 0, MPI_COMM_WORLD);
     } else {
-        // Done Floyd, recv proc_buffer, combine proc_buffers into `combined`.
+        // Recv proc_buffer, combine proc_buffers into `combined`.
         combined.resize(num_verts, std::vector<int>(num_verts));
         // Write proc 0 data into combined:
         int start_col = col_bounds[0];
@@ -154,14 +145,16 @@ void distrib(std::string input_file_path, std::string output_file_path) {
             }
         }
 
-        // Write all other proc data into `combined`:
+        // Make a buffer big enough to hold proc_buffer from any process:
+        int *recv_buffer = new int[num_verts * proc_width_max];
+
+        // Receive proc_buffer from all processes.
         for (int send_proc = 1; send_proc < world_size; send_proc++) {
             // How many ints to receive:
             int start_col = col_bounds[send_proc];
             int end_col = col_bounds[send_proc + 1];
             int buffer_len = (end_col - start_col) * num_verts;
 
-            int *recv_buffer = new int[buffer_len];
             MPI_Recv(recv_buffer, buffer_len, MPI_INT, send_proc, 
                 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             
@@ -176,12 +169,21 @@ void distrib(std::string input_file_path, std::string output_file_path) {
                 }
             }
 
-            delete[] recv_buffer;
         }
+        delete[] recv_buffer;
     }
 
-    // Root writes `combined` to file.
+    double proc_time = proc_timer.stop();
+    MPI_Gather(&proc_time, 1, MPI_DOUBLE, proc_times, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    // Root prints all times and writes `combined` to file.
     if (world_rank == 0) {
+        double root_time = root_timer.stop();
+        std::cout << "Time taken (in seconds) : \n" << std::setprecision(TIME_PRECISION);
+        for (int proc_rank = 0; proc_rank < world_size; proc_rank++) {
+            std::cout << proc_rank << ": "  << proc_times[proc_rank] << "\n";
+        }
+        std::cout << "Overall: " << root_time << "\n";
         graph_vector_to_file(combined, output_file_path);
         delete graph;
     }
